5d418e514dcf8a8ef12eba2fea285813ca242feb
komagata <komagata@gmail.com>
2024-05-31 02:22:42 +0900

    credentialを追加
    元々のパスワードがわからないため、新しく追加。

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,15 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.test?
-    provider :github, "client_id", "client_secret", scope: "read:user"
+    provider :github,
+      "client_id",
+      "client_secret",
+      scope: "read:user"
   elsif Rails.env.development?
-    provider :github, Rails.application.credentials.dev[:client_id], Rails.application.credentials.dev[:client_secret], scope: "read:user"
+    provider :github,
+      Rails.application.credentials.dev[:client_id],
+      Rails.application.credentials.dev[:client_secret],
+      scope: "read:user"
   else
     provider :github,
       Rails.application.credentials.github[:client_id],
